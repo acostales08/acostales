4ad8d7f667d4bbca4a61606a36617e0d268816ee
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-08-22 00:32:00 +0800

    skill and about motion

--- src/Components/Content/About/AboutSection.tsx
+++ src/Components/Content/About/AboutSection.tsx
@@ -8,7 +8,10 @@ const AboutSection: React.FC = () => {
     const strings = ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis dolor atque, quidem autem impedit provident aut quod, nobis ratione eveniet explicabo excepturi esse qui dolore perferendis veniam saepe quibusdam laudantium vero mollitia id delectus sint. Praesentium totam qui ut ab!'];
 
   return (
-    <motion.div animate={{x:-1000}} className='h-screen w-full ml-[1000px] bg-gradient-to-r from-[#1F2022] via-[#212225] to-[#121212] rounded-l-[100px] pt-16 px-28'>
+    <motion.div      
+        whileInView={{ x: 0, transition: { duration: 0.8 }, opacity: 1 }}
+        initial={{ x: 1000, opacity: 0 }}  
+        className='h-screen w-full  bg-gradient-to-r from-[#1F2022] via-[#212225] to-[#121212] rounded-l-[100px] pt-16 px-28'>
         <div className="w-full h-16  flex justify-center items-center">
             <h1 className="w-cover h-fit bg-clip-text bg-gradient-to-r from-[#FFD369] to-[rgba(255,210,105,0.31)] text-5xl font-bold text-transparent ">ABOUT ME</h1>          
         </div>
--- src/Components/Content/Skill/SkillSection.tsx
+++ src/Components/Content/Skill/SkillSection.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { Skillitems } from './Skillitem'
+import { motion } from 'framer-motion'
 
 
 const SkillSection: React.FC = () => {
   return (
-    <div className='h-auto w-full bg-gradient-to-r border-r-1 from-[#121212] via-[#212225] to-[#1F2022] rounded-r-[100px] py-16 px-20'>
+    <motion.div 
+        whileInView={{ x: 0, transition: { duration: 0.8 }, opacity: 1 }}
+        initial={{ x: -1000, opacity: 0 }} 
+        className='h-auto w-full bg-gradient-to-r border-r-1 from-[#121212] via-[#212225] to-[#1F2022] rounded-r-[100px] py-16 px-20'>
         <div className="w-full h-16  flex justify-center items-center ">
+        <motion.div
+          whileInView={{ y: 0, transition: { duration: 0.8 }, opacity: 1 }}
+          initial={{ y: 100, opacity: 0 }}
+        >
             <h1 className="w-cover h-fit bg-clip-text bg-gradient-to-r from-[rgba(255,210,105,0.31)] to-[#FFD369] text-5xl font-bold text-transparent ">Skills and Tools</h1>          
+        </motion.div>
         </div>
-        <div className=" w-full h-fit flex justify-center py-10 text-1xl">
+        <motion.div
+          whileInView={{ y: 0, transition: { duration: 0.8 }, opacity: 1 }}
+          initial={{ y: -100, opacity: 0 }}
+          className=" w-full h-fit flex justify-center py-10 text-1xl"
+        >
             <p className="text-[#EEEEEE]  w-[70%] text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolores asperiores natus. Labore dolorem nobis sunt vero, quidem reprehenderit. Cum autem possimus aspernatur id quae deleniti nemo, doloribus iste. Sunt.</p>
-        </div>
+        </motion.div>
         <div className=" mt-10 h-auto w-full flex flex-wrap gap-4 p-5 justify-center items-center">
             {Skillitems.map(({img}, index) => (
                 <div key={index} className="flex justify-center items-center w-[100px] h-[100px]  rounded-2xl bg-[#EEEEEE] shadow-xl">
@@ -19,7 +32,7 @@ const SkillSection: React.FC = () => {
             ))}
 
         </div>
-    </div>
+    </motion.div>
   )
 }
 
